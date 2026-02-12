fcfb7894591817740a57c02b8098f368bdfcb8ff
barbaraogalde-wq <barbara.ogalde@gmail.com>
2026-02-12 12:02:09 -0300

    Create BotonWhatsApp.js

---
+++ app/components/BotonWhatsApp.js
@@ -0,0 +1,10 @@
+export default function BotonWhatsApp() {
+  return (
+    <a
+      href="https://wa.me/569XXXXXXXX"
+      className="fixed bottom-6 right-6 bg-green-500 p-4 rounded-full shadow-lg"
+    >
+      💬
+    </a>
+  );
+}
