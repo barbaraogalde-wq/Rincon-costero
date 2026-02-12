b59a2380df5fbcf46854baffc0c6da7efef99ebb
barbaraogalde-wq <barbara.ogalde@gmail.com>
2026-02-12 11:53:59 -0300

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,23 @@
+rincon-costero/
+│
+├── app/
+│   ├── layout.js
+│   ├── page.js
+│
+├── components/
+│   ├── Hero.js
+│   ├── Beneficios.js
+│   ├── Galeria.js
+│   ├── Reservas.js
+│   ├── Testimonios.js
+│   ├── Ubicacion.js
+│   ├── Footer.js
+│   ├── BotonWhatsApp.js
+│
+├── public/
+│   ├── images/
+│
+├── styles/
+│
+├── tailwind.config.js
+├── package.json
